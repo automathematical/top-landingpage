402ca9a4cbf1a3d818695b859405832132552c48
automathematical <vanleemputtenmathieu@gmail.com>
2024-04-10 21:48:13 +0700

    c

--- src/main.jsx
+++ src/main.jsx
@@ -2,15 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Experience from './Components/Experience.jsx'
 import { Overlay } from './Components/Overlay.jsx'
-
 import { Leva } from 'leva'
 
 import './index.css'
-import GoogleAnalytics from './Components/GoogleAnalytics.jsx'
+import ReactGA from 'react-ga4'
+ReactGA.initialize('G-WR3E9BJWNP')
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <GoogleAnalytics />
     <Experience />
     <Overlay />
     <Leva hidden />

ae1bbb974690b1c10ad7c473b13f7a6bbf4882a8
automathematical <vanleemputtenmathieu@gmail.com>
2024-04-10 21:55:23 +0700

    c

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,10 @@ import ReactDOM from 'react-dom/client'
 import Experience from './Components/Experience.jsx'
 import { Overlay } from './Components/Overlay.jsx'
 import { Leva } from 'leva'
+import ReactGA from 'react-ga4'
 
 import './index.css'
-import ReactGA from 'react-ga4'
+
 ReactGA.initialize('G-WR3E9BJWNP')
 
 ReactDOM.createRoot(document.getElementById('root')).render(

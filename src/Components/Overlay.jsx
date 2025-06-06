532641031583a6560fb58e3bfb5b27c8ba0c12f7
automathematical <vanleemputtenmathieu@gmail.com>
2025-06-06 21:11:12 +0700

    changes

--- src/Components/Overlay.jsx
+++ src/Components/Overlay.jsx
@@ -3,17 +3,13 @@ import Input from './Input'
 export function Overlay() {
   return (
     <>
-      <div
-        style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}></div>
+      <div style={{ position: 'absolute', top: 0, left: 0, pointerEvents: 'none', width: '100%', height: '100%' }}></div>
       <div style={{ position: 'absolute', top: 40, left: 40, fontSize: '13px' }}>
         <p>
-          <a href='https://timeoffprojects.com/contact'>Contact</a>
+          <a href='https://timeoffprojects.com/#contact'>Contact</a>
         </p>
       </div>
       <div style={{ position: 'absolute', top: 40, right: 40, fontSize: '13px' }}>
-        <p>
-          <a href=''>Digital Playground</a>
-        </p>
         <p>
           <a href='https://github.com/automathematical/top-landingpage'>Get the code</a>
         </p>

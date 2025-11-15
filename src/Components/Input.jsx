c0d123cc213f3f08a4a9d2980e7cb86f485e9aa9
automathematical <vanleemputtenmathieu@gmail.com>
2025-11-15 14:57:20 +0700

    fix: update initial input text for better user experience

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -49,7 +49,7 @@ const styles = {
 }
 
 export default function Input() {
-  const [text, setText] = React.useState('write whatever')
+  const [text, setText] = React.useState('Can not wait to try this out!')
   const [isFocused, setIsFocused] = React.useState(false)
   const [isHovered, setIsHovered] = React.useState(false)
 
